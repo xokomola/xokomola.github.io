982f49ae398c2460908616a6955fc8e432c0e5f3
Marc van Grootel <marc.van.grootel@gmail.com>
2025-09-20 19:03:23 +0200

    Add Astro GH action

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,7 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://xokomola.github.io',
 	integrations: [mdx(), sitemap()],
 });
